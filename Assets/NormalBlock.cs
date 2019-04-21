0c5ba32d750d0b9605e7b6caed059df4b78b724d
Harry Cai <harry.cai@hotmail.com>
2019-04-21 14:48:22 +0100

    Delete block on collision, make objects into prefabs

---
+++ Assets/NormalBlock.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NormalBlock : MonoBehaviour
+{
+    private void OnCollisionExit(Collision other)
+    {
+        if (other.gameObject.tag == "Ball")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
