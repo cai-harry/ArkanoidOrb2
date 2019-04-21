368c837d3a0a56187e57ed144810c9f4db61b896
Harry Cai <harry.cai@hotmail.com>
2019-04-21 20:04:00 +0100

    Random instantiation of NormalBlocks

---
+++ Assets/MainScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainScript : MonoBehaviour
+{
+    public float firstBlockSeconds;
+    public float addBlockRate;
+    public float blockSpaceRadius;
+    
+    public GameObject normalBlock;
+
+    void Start()
+    {
+        InvokeRepeating("AddNormalBlock", firstBlockSeconds, addBlockRate);
+    }
+    
+    private void AddNormalBlock()
+    {
+        // TODO: don't instantiate on top of other blocks or balls
+        var randomUnitCirclePosition = Random.insideUnitCircle;
+        var startPosition = new Vector3(
+            blockSpaceRadius * randomUnitCirclePosition.x,
+            normalBlock.transform.position.y,
+            blockSpaceRadius * randomUnitCirclePosition.y
+        );
+        Instantiate(normalBlock, startPosition, Quaternion.identity);
+    }
+
+    
+}
