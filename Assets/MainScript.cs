e3da2f2747bf57918f8085fb0e55eb8c1166cb4e
Harry Cai <harry.cai@hotmail.com>
2019-04-22 16:11:35 +0100

    ESC key to pause / unpause

--- Assets/MainScript.cs
+++ Assets/MainScript.cs
@@ -7,14 +7,54 @@ public class MainScript : MonoBehaviour
     public float firstBlockSeconds;
     public float addBlockRate;
     public float blockSpaceRadius;
-    
+
     public GameObject normalBlock;
     public GameObject rockBlock;
     public GameObject ballBlock;
 
+    public AudioSource strikebeamSong;
+
+    public GameObject pauseScreen;
+    private bool paused;
+
     void Start()
     {
         InvokeRepeating("AddRandomBlock", firstBlockSeconds, addBlockRate);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Escape))
+        {
+            TogglePause();
+        }
+    }
+
+    private void TogglePause()
+    {
+        if (paused)
+        {
+            Unpause();
+            return;
+        }
+
+        Pause();
+    }
+
+    private void Pause()
+    {
+        strikebeamSong.Pause();
+        Time.timeScale = 0f;
+        paused = true;
+        Instantiate(pauseScreen);
+    }
+
+    private void Unpause()
+    {
+        strikebeamSong.UnPause();
+        Time.timeScale = 1f;
+        paused = false;
+        Destroy(GameObject.FindWithTag("PauseScreen"));
     }
 
     private void AddRandomBlock()
@@ -33,7 +73,7 @@ public class MainScript : MonoBehaviour
             InstantiateBlock(normalBlock);
         }
     }
-    
+
     private void InstantiateBlock(GameObject block)
     {
         // TODO: don't instantiate on top of other blocks or balls
@@ -45,5 +85,4 @@ public class MainScript : MonoBehaviour
         );
         Instantiate(block, startPosition, Quaternion.identity);
     }
-    
 }
