a69dd8cd013cb5305e18acce849b57cc368154be
Harry Cai <harry.cai@autodesk.com>
2019-05-08 12:41:28 +0100

    Add lightning to MagnetBlock
    Currently follows a ball

--- Assets/MagnetBlock.cs
+++ Assets/MagnetBlock.cs
@@ -1,12 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Experimental.PlayerLoop;
 
 public class MagnetBlock : NormalBlock
 {
     public float minRepulsion;
     public float maxRepulsion;
     public float magneticRadius;
+
+    public ParticleSystem lightning;
 
     private float _repulsionForce;
 
@@ -22,6 +25,11 @@ public class MagnetBlock : NormalBlock
         {
             var ballRigidBody = ball.gameObject.GetComponent<Rigidbody>();
             ballRigidBody.AddExplosionForce(_repulsionForce, transform.position, magneticRadius);
+            var lightningVelocity = lightning.velocityOverLifetime;
+            var positionDeltaToBall = ball.transform.position - transform.position;
+            lightningVelocity.x = positionDeltaToBall.x / ball.transform.localScale.x;
+            lightningVelocity.y = positionDeltaToBall.y / ball.transform.localScale.y;
+            lightningVelocity.z = positionDeltaToBall.z / ball.transform.localScale.z;
         }
     }
 }
