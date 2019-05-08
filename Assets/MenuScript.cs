5b657bcbfd9989eba68217a400e913a9349cc6d9
Harry Cai <harry.cai@autodesk.com>
2019-05-08 15:27:32 +0100

    Press Enter on intro scene to load the game scene

---
+++ Assets/MenuScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour
+{
+    public string gameSceneName;
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Return))
+        {
+            LoadGameScene();
+        }
+    }
+
+    private void LoadGameScene()
+    {
+        Debug.Log("Loading game scene");
+        SceneManager.LoadScene(gameSceneName);
+    }
+}
