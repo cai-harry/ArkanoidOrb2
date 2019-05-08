46da9aab8458699d336898d55f3b4b6b2704df9d
Harry Cai <harry.cai@autodesk.com>
2019-05-08 14:09:55 +0100

    Lightning: on each frame, pick random ball in range, go towards it

--- Assets/MagnetBlock.cs
+++ Assets/MagnetBlock.cs
@@ -10,13 +10,53 @@ public class MagnetBlock : NormalBlock
     public float magneticRadius;
 
     public ParticleSystem lightning;
+    public Vector3 lightningVelocityMultiplier;
 
+    private ParticleSystem.VelocityOverLifetimeModule _lightningVelocity;
     private float _repulsionForce;
 
     protected override void Start()
     {
         base.Start();
         _repulsionForce = Random.Range(minRepulsion, maxRepulsion);
+        _lightningVelocity = lightning.velocityOverLifetime;
+    }
+
+    private void Update()
+    {
+        List<GameObject> ballsInRange = new List<GameObject>();
+        foreach (var ball in GameObject.FindGameObjectsWithTag("Ball"))
+        {
+            var positionDeltaToBall = ball.transform.position - transform.position;
+            if (positionDeltaToBall.magnitude < magneticRadius)
+            {
+                ballsInRange.Add(ball);
+            }
+        }
+
+        if (ballsInRange.Count > 0)
+        {
+            var ball = ChooseRandomFrom(ballsInRange);
+            var positionDeltaToBall = ball.transform.position - transform.position;
+            _lightningVelocity.x =
+                lightningVelocityMultiplier.x * positionDeltaToBall.x / ball.transform.localScale.x;
+            _lightningVelocity.y =
+                lightningVelocityMultiplier.y * positionDeltaToBall.y / ball.transform.localScale.y;
+            _lightningVelocity.z =
+                lightningVelocityMultiplier.z * positionDeltaToBall.z / ball.transform.localScale.z;
+        }
+        else
+        {
+            _lightningVelocity.x = 0;
+            _lightningVelocity.y = 0;
+            _lightningVelocity.z = 0;
+        }
+    }
+
+    private static GameObject ChooseRandomFrom(List<GameObject> list)
+    {
+        var randomIndex = Random.Range(0, list.Count);
+        return list[randomIndex];
     }
 
     private void FixedUpdate()
@@ -25,11 +65,6 @@ public class MagnetBlock : NormalBlock
         {
             var ballRigidBody = ball.gameObject.GetComponent<Rigidbody>();
             ballRigidBody.AddExplosionForce(_repulsionForce, transform.position, magneticRadius);
-            var lightningVelocity = lightning.velocityOverLifetime;
-            var positionDeltaToBall = ball.transform.position - transform.position;
-            lightningVelocity.x = positionDeltaToBall.x / ball.transform.localScale.x;
-            lightningVelocity.y = positionDeltaToBall.y / ball.transform.localScale.y;
-            lightningVelocity.z = positionDeltaToBall.z / ball.transform.localScale.z;
         }
     }
 }
